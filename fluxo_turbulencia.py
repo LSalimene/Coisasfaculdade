8605b7f2ac36deda131a1de27cc2cd521bde4102
Lucas <lucassalimene@gmail.com>
2020-10-22 13:32:55 -0300

    agora vai

--- fluxo_turbulencia.py
+++ fluxo_turbulencia.py
@@ -4,12 +4,16 @@ import pandas as pd
 #dados do dia 2 de maio de 2014
 dados = pd.read_csv(('L_2014_122_1200.dat'),header=None)
 #importar cada variavel
-u1 = pd.DataFrame(dados,columns=[4])
-v1 = pd.DataFrame(dados,columns=[5])
+
 w1 = pd.DataFrame(dados,columns=[6])
 ts = pd.DataFrame(dados,columns=[7])
-n_bins = 5
+n_bins = 10
 #plotar a imagem
-plt.hist(u1, bins=n_bins, density=True)
+def truncate(n, decimals=0):
+    multiplier = 10 ** decimals
+    return int(n * multiplier) / multiplier
+#a
+
+plt.hist(w1r)
 
 plt.show()
