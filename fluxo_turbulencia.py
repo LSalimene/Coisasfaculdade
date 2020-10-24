0fc2466fbe04e37bb16840712e2fb56e7a231fbe
Lucas <lucassalimene@gmail.com>
2020-10-24 18:04:40 -0300

    arrumado com o np.transpose

--- fluxo_turbulencia.py
+++ fluxo_turbulencia.py
@@ -4,16 +4,12 @@ import pandas as pd
 #dados do dia 2 de maio de 2014
 dados = pd.read_csv(('L_2014_122_1200.dat'),header=None)
 #importar cada variavel
-
 w1 = pd.DataFrame(dados,columns=[6])
 ts = pd.DataFrame(dados,columns=[7])
-n_bins = 10
 #plotar a imagem
-def truncate(n, decimals=0):
-    multiplier = 10 ** decimals
-    return int(n * multiplier) / multiplier
-#a
+w1o=np.transpose(ts)
 
-plt.hist(w1r)
-
+plt.hist(w1o,bins=25,color='r')
+plt.xlabel('T (C)')
+plt.ylabel('Frequência de Ocorrencia')
 plt.show()
