4e1a48956f233964fabd0bbf70d3c0629e46508c
Lucas <lucassalimene@gmail.com>
2020-10-24 21:30:06 -0300

    histogramas com a diferença da media de w1 e ts

--- fluxots.py
+++ fluxots.py
@@ -7,12 +7,12 @@ dados = pd.read_csv(('L_2014_122_1200.dat'),header=None)
 w1 = pd.DataFrame(dados,columns=[6])
 ts = pd.DataFrame(dados,columns=[7])
 #arrumar os dados
-w1s=np.transpose(w1)
+tss=np.transpose(ts)
 #calcular a media
-w1m=np.mean(w1)
-w1d=w1s+int(w1m)
+tsm=np.mean(ts)
+tsd=tss-int(tsm)
 #plotar a imagem
-plt.hist(w1d,bins=35,color='b')
+plt.hist(tsd,bins=35,color='r')
 plt.xlabel('T (C)')
 plt.ylabel('Frequência de Ocorrencia')
 plt.show()
