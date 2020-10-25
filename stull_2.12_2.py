899d9f6aeaad5f84ed30b6ccb91b92c45fffe03e
Lucas <lucassalimene@gmail.com>
2020-10-25 12:48:14 -0300

    correção nas contas

--- stull_2.12_2.py
+++ stull_2.12_2.py
@@ -4,10 +4,16 @@ import pandas as pd
 #Dados do exercicio
 w=[0.5,-0.5,1,0.8,0.9,-0.2,-0.5,0,-0.9,-0.1]
 t=[295,293,295,298,292,294,292,289,293,299]
+#Contas pedidas no exercico
 ws=np.mean(w)
 wm=w-ws
 ts=np.mean(t)
 tm=t-ts
 w2=np.multiply(wm,wm)
 t2=np.multiply(tm,tm)
-print(w,t,ws,ts,w2,t2,wm,tm)
+wt=np.multiply(w,t)
+wtd=np.multiply(wm,tm)
+wt_total=wt+wtd
+ubd=np.std(t) #unbiased standard deviation
+r2=np.corrcoef(t,w)
+print(r2)
