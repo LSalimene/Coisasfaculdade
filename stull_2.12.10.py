73d7c256523b1811d6f005d84858e0ddb6978d5c
Lucas <lucassalimene@gmail.com>
2020-11-07 18:05:57 -0300

    grafico

--- stull_2.12.10.py
+++ stull_2.12.10.py
@@ -7,15 +7,16 @@ u = [3,4,3,6, 6, 2, 5, 3, 7, 8, 5, 4, 3, 5, 2, 2, 4, 5, 3, 6, 5]
 u1 = [3,4,3,6, 6, 2, 5, 3, 7, 8, 5, 4, 3, 5, 2, 2, 4, 5, 3, 6, 5]
 umean = np.mean(u)
 u2 = np.subtract(u,u1)
+u3 = u - umean
 um = u2 + (umean)
 plt.xlim (0,60)
-
-plt.plot(t,u,label="t vs U")
+#plt.plot(t,u,label="t vs U")
 plt.hold = True
 print(umean)
-plt.plot(t,um,label='p vs Ū')
+plt.plot(t,um,label=('p vs Ū'))
+plt.plot(t,u3,label=("p vs U'")) 
 plt.xlabel ('Tempo (s)')
 plt.ylabel ('U (m/s)')
-plt.legend()
+plt.legend(loc='best')
 plt.grid()
 plt.show()
