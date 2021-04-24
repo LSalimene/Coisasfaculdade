79ec685a3a44d9df4ddfb678e127cd215effdb09
Lucas <lucassalimene@gmail.com>
2021-04-24 12:15:12 -0300

    label arrumada

--- fisica_computacional/teste.py
+++ fisica_computacional/teste.py
@@ -2,9 +2,9 @@ import numpy as np #importar o pacote numpy
 from matplotlib import pyplot as plt #importar a função pyplot do pacote matplotlib
 
 #condições iniciais para o metodo de Euler
-x0 = 0 #criar um intervalo entre 0 e 10 para a constante de decaimento
+x0 = 0 #criar um intervalo entre 0 e 10 para o tempo
 n0 = 1 #valor inicial de atomos no instante t
-xf = 10 #criar um intervalo entre 0 e 10 para a constante de decaimento
+xf = 10 
 p = 1000 #numero de pontos
 deltax = (xf-x0)/(p-1)
 
@@ -17,11 +17,9 @@ n[0] = n0
 for i in range (1,p):
    n[i] = deltax*(-n[i-1] *x[i-1]) + n[i-1]
 
-for i in range (p):
-   print(x[i],n[i])
 
 plt.plot(x,n,'g.')
-plt.xlabel('Valor de beta')
-plt.ylabel('Numero de atomos no tempo t')
-plt.title ( ' Solução aproximada pela metodo de Euler ' )
+plt.xlabel('Tempo em minutos')
+plt.ylabel('Numero de atomos')
+plt.title ( ' Solução aproximada pelo metodo de Euler ' )
 plt.show()
